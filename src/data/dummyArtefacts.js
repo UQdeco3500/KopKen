117d8bdc184bacbfdf67b8222a3a02afc8b34793
hazlazuardi <hazlazuardi@gmail.com>
2023-10-09 03:11:29 +1000

    feat: display artefacts based on user's location

---
+++ src/data/dummyArtefacts.js
@@ -0,0 +1,106 @@
+export const dummyArtefacts = [
+    {
+        type: 'photo',
+        content: require('./photos/Iglu/photo1.jpeg'),
+        contexts: {
+            location: {
+                name: 'Iglu Brisbane CBD',
+                latitude: -27.472679399680185,
+                longitude: 153.02711120711072
+            },
+        },
+    },
+    {
+        type: 'photo',
+        content: require('./photos/Iglu/photo2.jpeg'),
+        contexts: {
+            location: {
+                name: 'Iglu Brisbane CBD',
+                latitude: -27.472679399680185,
+                longitude: 153.02711120711072
+            },
+        },
+    },
+    {
+        type: 'photo',
+        content: require('./photos/Iglu/photo3.jpeg'),
+        contexts: {
+            location: {
+                name: 'Iglu Brisbane CBD',
+                latitude: -27.472679399680185,
+                longitude: 153.02711120711072
+            },
+        },
+    },
+    {
+        type: 'photo',
+        content: require('./photos/AndrewLiveris/photo9.jpeg'),
+        contexts: {
+            location: {
+                name: 'Andrew N. Liveris',
+                latitude: -27.499143723038042,
+                longitude: 153.01390536053944
+                    },
+        },
+    },
+    {
+        type: 'photo',
+        content: require('./photos/AndrewLiveris/photo10.jpeg'),
+        contexts: {
+            location: {
+                name: 'Andrew N. Liveris',
+                latitude: -27.499143723038042,
+                longitude: 153.01390536053944
+                    },
+        },
+    },
+    {
+        type: 'story',
+        content: {
+            title: 'Winter Gloves',
+            content: 'This is a story about two lovers..'
+        },
+        contexts: {
+            location: {
+                name: 'Andrew N. Liveris',
+                latitude: -27.499143723038042,
+                longitude: 153.01390536053944
+                    },
+        },
+    },
+    {
+        type: 'photo',
+        content: require('./photos/AndrewLiveris/photo11.jpeg'),
+        contexts: {
+            location: {
+                name: 'Andrew N. Liveris',
+                latitude: -27.499143723038042,
+                longitude: 153.01390536053944
+                    },
+        },
+    },
+    {
+        type: 'photo',
+        content: require('./photos/AndrewLiveris/photo12.jpeg'),
+        contexts: {
+            location: {
+                name: 'Andrew N. Liveris',
+                latitude: -27.499143723038042,
+                longitude: 153.01390536053944
+                    },
+        },
+    },
+    {
+        type: 'keyword',
+        content: 'Kebab Turqo',
+        contexts: {
+            location: {
+                name: 'Andrew N. Liveris',
+                latitude: -27.499143723038042,
+                longitude: 153.01390536053944
+                    },
+        },
+    },
+
+    // ... other artifact objects
+];
